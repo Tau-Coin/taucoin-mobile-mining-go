45c17113c373978ecb383c304803fe8a5ecb1a6f
shunli.wang <shunli.wang@dwfund.net>
2020-04-29 14:15:52 +0800

    update genesis spec

--- core/genesis/doc.go
+++ core/genesis/doc.go
@@ -14,11 +14,11 @@
 // for more details of LGPL, see <http://www.gnu.org/licenses/>.
 // maintained by likeopen
 //*********************************************************//
-//1.0 how to construct a genesis contract
-//2.0 if genesis contract was created, other variable will be populated
+//1.0 how to construct a genesis block
+//2.0 if genesis block was created, other variable will be populated
 //*********************************************************//
 
-// 1.0 how to construct a genesis contract Y
+// 1.0 how to construct a genesis block g
 // 1.1 check aplication current runing context (background)
 // 1.2 background will tell you wheather there are some contract chains in app.
 // 1.3 if there are chains in background, using tau address private key pair existing
@@ -29,55 +29,29 @@
 // 1.5 please specify below value a config file ?
 // 	version uint64
 // 	timestampinrelaytimeunit uint32 (example current unix time 1500000 to 1500015 will have same birthday unix/15)
-// 	contractnum int32
-// 	(
-// 	nickname [32]byte, blocktime uint8 default 5min, sign timestampinrelaytimeunit with tau private key
-// 	marked sig
-// 	hash(sig)
-// 	)
-// 	chainid [32 + 1 + 32]byte
-// 	safetycontractresultroot nil
+// 	blocknum int32 default 0
+// 	previousblockroot nil
 // 	basetarget [32]byte default ?
 // 	cummulativedifficulty int64 / [32]byte default = 0 at here.
 // 	generationSignature [32]byte random
-// 	txfee uint64 default = 10 000 000 total suply
-// 	nonce uint64 default = 0 minerpower, if initial is 0 lead num 0 contract will mined when selfminingtime=60min timeout
-// 	ipfssigon [65]byte node ipfs private key sign tauaddr to show relationship ipfs and tauaddress locally.
-// 	msg []byte contains optional msg like your vision
+// 	IPLDsigon [65]byte node ipfs private key sign tauaddr to show relationship ipfs and tauaddress locally.
+//msg []byte contains initalK-V that is initial state. nonce is previous value in tau and 0 in others, 1th block is allowed to be mined with 0
+// 	chainid [32 + 32]byte (nickname [32]byte,hash(timestampinrelaytimeunit and tau private key))
 // 	signature tau private key sign this contract ,others can recover genesis tau address
-// 1.6 construct genesis contract Y if before have no error
-// 2.0 execuate genesis will update hamt,temporary vaiable and db locally.
-// 	hamt:
+// 1.6 construct genesis block g if before have no error
+// 2.0 execuate genesis will update IPLD block store temporary variable and leveldb locally.
+// 	IPLD blockchain(blockstore):
 // 	1 contract per hamt here will be create , and update from now on.
-// 	hamt := new hamt()
-// 	if (!isTau()){
-// 		hamt.add(contractJSON,Y)
-// 		hamt.add(genesistauaddressbalance, above specify value)
-// 		hamt.add(genesistauaddressnone,0)
-//      hamt.add(relaynonce,count of relay from relay announce msg if exist)
-//      hamt.add(relaynonceaddress, relay msg mutiaddr if exist)
-// 		contractresultstateroot = hamt.add(genesistauaddressNoncetxJSON,nil) //empty tx in genesis contract
-// 	}else{
-// 		hamt.add(contractJSON,Y)
-// 		for i:=0;i < 13w;i++{
-// 			hamt.add(genesistauaddress[i]balance, above specify value)
-// 			hamt.add(genesistauaddress[i]nonce,0)
-// 		}
-//      hamt.add(relaynonce,count of relay from relay announce msg tau default)
-//      hamt.add(relaynonceaddress, relay msg mutiaddr tau default)
-// 		contractresultstateroot = hamt.add(genesistauaddressNoncetxJSON,nil) //empty tx in genesis contract
-// 	}
+// 	bs := new blockstore()
+//      rootcid = bs.put(cbor.encode(g))
 
 // 	temporary vaiable:
 // 	currentchainid = above got.
 
 // 	db:
-// 	mychainsdb.put(chainid,...)
-// 	mycontractresultstaterootdb.put(chainid,contractresultstateroot)
-// 	mysafetycontractresultstaterootdb.put(chainid,nil)
-// 	mysafetycontractresultstaterootminerdb.put(chainid,genesistauaddress)
-// 	myprevioussafetycontractresultstaterootminerdb.put(chainid,nil)
-//  mypeersdb.put(chianid,addres included taugenesis and ipfs besides config)
-//  myrelaysdb.put(chaind,relay multiaddr and config) //keep ratio 2:1:7 to get network alive healthy,from 3 aspect
-//  taurealy(hamt), ownrelay(hamt), succeedrelay(active process)
+// 	dbChains.put(chainid,...)
+// 	dbBlockRoot.put(chainid,rootcid)
+//      dbState.put(chainid,state associated to tauaddress)//fix me
+//      dbPeers.put(chianid,addres included taugenesis and ipfs besides config)
+//      dbRelay.put(chaind,relay multiaddr and config) //keep ratio 2:1:7 to get network alive healthy,from 3 aspect
 package genesis
